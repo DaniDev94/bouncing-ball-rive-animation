18cebb19c914fe6b41494f20efb4b45410daf655
Daniel López <daniel.lp1094@gmail.com>
2023-01-27 09:24:42 +0100

    [ 1.0.1 => move the file index.html to the root]

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-// import rive from "@rive-app/canvas";
+import rive from "@rive-app/canvas";
+
+new rive.Rive({
+    src: "https://cdn.rive.app/animations/vehicles.riv",
+    // Or the path to a local Rive asset
+    // src: './example.riv',
+    canvas: document.getElementById("canvas"),
+    autoplay: true
+});
