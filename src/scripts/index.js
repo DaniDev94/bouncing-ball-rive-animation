e87f082b0284606824262319d59014a70dafcc3b
Daniel López <daniel.lp1094@gmail.com>
2023-02-03 10:04:06 +0100

    [ 3.0.0 => Finished layout and river animations added successfully ]

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,55 @@
 
-function riveObject() {
-    return new rive.Rive({
-        src: 'src/assets/exports/ball_1.riv',
-        canvas: document.getElementById("canvas"),
+function riveObjects() {
+    new rive.Rive({
+        src: 'src/assets/exports/ball_0.riv',
+        canvas: document.getElementById("ball-0"),
         autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+
+    new rive.Rive({
+        src: 'src/assets/exports/ball_1.riv',
+        canvas: document.getElementById("ball-1"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+
+    new rive.Rive({
+        src: 'src/assets/exports/ball_2.riv',
+        canvas: document.getElementById("ball-2"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+
+    new rive.Rive({
+        src: 'src/assets/exports/stars.riv',
+        canvas: document.getElementById("stars"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+
+    new rive.Rive({
+        src: 'src/assets/exports/heart.riv',
+        canvas: document.getElementById("heart-green"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+
+    new rive.Rive({
+        src: 'src/assets/exports/heart.riv',
+        canvas: document.getElementById("heart-purple"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
+    })
+    
+    new rive.Rive({
+        src: 'src/assets/exports/heart.riv',
+        canvas: document.getElementById("heart-blue"),
+        autoplay: true,
+        layout: new rive.Layout({ fit: 'cover', alignment: 'center' }),
     })
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    riveObject();
+    riveObjects();
 });
