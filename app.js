039c064e8e55ca0afa69b1158d438dc0a36b28ff
Daniel López <daniel.lp1094@gmail.com>
2023-05-24 13:44:23 +0200

    [6.1.4 => fix paths error in file app.js]

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ const app = express();
 const PORT = 8080;
 
 // Server configuration for load static assets and css files
-app.use('/exports', express.static(path.join(__dirname, 'exports')))
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/styles', express.static(path.join(__dirname, 'styles')))
-app.use('/src/action_menu', express.static(path.join(__dirname, 'src/action_menu')))
-app.use('/src/basic_concepts', express.static(path.join(__dirname, 'src/basic_concepts')))
-app.use('/src/state_machines', express.static(path.join(__dirname, 'src/state_machines')))
+app.use('/src/utils/exports', express.static(path.join(__dirname, 'src/utils/exports')))
+app.use('/src/utils/styles', express.static(path.join(__dirname, 'src/utils/styles')))
+app.use('/src/pages/action_menu', express.static(path.join(__dirname, 'src/pages/action_menu')))
+app.use('/src/pages/basic_concepts', express.static(path.join(__dirname, 'src/pages/basic_concepts')))
+app.use('/src/pages/state_machines', express.static(path.join(__dirname, 'src/pages/state_machines')))
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/action_menu/index.html'));
+    res.sendFile(path.join(__dirname, '/src/pages/action_menu/index.html'));
 });
 
 app.get('/basic', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/basic_concepts/index.html'));
+    res.sendFile(path.join(__dirname, '/src/pages/basic_concepts/index.html'));
 });
 
 app.get('/machines', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/state_machines/index.html'));
+    res.sendFile(path.join(__dirname, '/src/pages/state_machines/index.html'));
 });
 
 
